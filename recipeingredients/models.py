9dbaf1930943eb0fc6c195b725b114df33e8cb8d
jdussold <jdussold@gmail.com>
2023-07-16 18:54:56 -0500

    updated to include the "related_name" attribute

--- recipeingredients/models.py
+++ recipeingredients/models.py
@@ -1,13 +1,11 @@
 from django.db import models
-from recipes.models import Recipe
-from ingredients.models import Ingredient
-
-# Create your models here.
 
 
 class RecipeIngredient(models.Model):
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(
+        "recipes.Recipe", on_delete=models.CASCADE, related_name="recipe_ingredients"
+    )
+    ingredient = models.ForeignKey("ingredients.Ingredient", on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.ingredient} - {self.recipe}"
