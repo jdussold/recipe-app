c7560449b9d09e171f56d5347d866de9b294f43c
jdussold <jdussold@gmail.com>
2023-07-16 18:58:44 -0500

    created list and detail views

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Recipe
 
 
 # Create your views here.
 def recipes_home(request):
     return render(request, "recipes/recipes_home.html")
+
+
+class RecipesListView(ListView):
+    model = Recipe
+    template_name = "recipes/recipes_list.html"
+    context_object_name = "recipes"
+
+
+class RecipesDetailView(DetailView):
+    model = Recipe
+    template_name = "recipes/recipes_detail.html"
+    context_object_name = "recipe"
