e1b2b823b6d075143ce099c18a919d6a4401ee41
jdussold <jdussold@gmail.com>
2023-07-24 10:00:51 -0500

    added urls for charts and exports

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from .views import recipes_home, RecipesListView, RecipesDetailView
+from .views import (
+    recipes_home,
+    RecipesListView,
+    RecipesDetailView,
+    export_recipes_csv,
+    generate_chart,
+)
 
 app_name = "recipes"
 
 urlpatterns = [
     path("", recipes_home, name="recipes_home"),
     path("recipes/", RecipesListView.as_view(), name="recipes_list"),
-    path("recipes/<pk>", RecipesDetailView.as_view(), name="recipes_detail"),
+    path("recipes/export/", export_recipes_csv, name="export_csv"),
+    path("generate-chart/", generate_chart, name="generate_chart"),
+    path("recipes/<pk>/", RecipesDetailView.as_view(), name="recipes_detail"),
 ]
