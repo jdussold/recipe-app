69b4646780e0e937c7f202e299cf3fa4deb34319
jdussold <jdussold@gmail.com>
2023-07-28 17:57:05 -0500

    Updated ingredients model

--- ingredients/models.py
+++ ingredients/models.py
@@ -1,6 +1,6 @@
-from django.db import models
+# ingredients/models.py
 
-# Create your models here.
+from django.db import models
 
 
 class Ingredient(models.Model):
@@ -8,3 +8,19 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(
+        "recipes.Recipe", on_delete=models.CASCADE, related_name="ingredients_used"
+    )
+    ingredient = models.ForeignKey(
+        "ingredients.Ingredient",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="recipes_used",
+    )
+
+    def __str__(self):
+        return f"{self.ingredient} - {self.recipe}"
