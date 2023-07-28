d42bd1cda302c6c75c0b776a7393cf82ead8987e
jdussold <jdussold@gmail.com>
2023-07-28 18:03:04 -0500

    updated urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,14 +5,18 @@ from .views import (
     RecipesDetailView,
     export_recipes_csv,
     generate_chart,
+    about_page,
+    add_recipe,
 )
 
 app_name = "recipes"
 
 urlpatterns = [
     path("", recipes_home, name="recipes_home"),
-    path("recipes/", RecipesListView.as_view(), name="recipes_list"),
+    path("recipes/add/", add_recipe, name="add_recipe"),
     path("recipes/export/", export_recipes_csv, name="export_csv"),
     path("generate-chart/", generate_chart, name="generate_chart"),
-    path("recipes/<pk>/", RecipesDetailView.as_view(), name="recipes_detail"),
+    path("recipes/<int:pk>/", RecipesDetailView.as_view(), name="recipes_detail"),
+    path("about/", about_page, name="about"),
+    path("recipes/", RecipesListView.as_view(), name="recipes_list"),
 ]
